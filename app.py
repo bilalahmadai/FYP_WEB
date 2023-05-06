37366f0eebb4bb8a41228091b562c63db3716287
bilalansar3 <bilalahmadai971@gmail.com>
2023-05-06 14:18:18 +0500

    face and web

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask,render_template,Response,url_for
 import cv2
+from mySQL import *
+import cv2 as cv
+
+import pickle
+import face_recognition
+import numpy as np
+import cvzone 
 
 app=Flask(__name__)
 camera=cv2.VideoCapture(0)
+print("Encoded File Loading...")
+file=open("EncodeFile.p","rb")
+model=pickle.load(file)
+file.close()
+print("Encoded File Loaded")
+knownEncodeList,studentIDs = model
 
 def generate_frames():
     while True:
@@ -12,6 +25,47 @@ def generate_frames():
         if not success:
             break
         else:
+            #face recognition----
+            imgS=cv.resize(frame,(0,0),None,0.25,0.25)
+            imgS=cv.cvtColor(imgS,cv.COLOR_BGR2RGB)
+
+            faceCurLoc=face_recognition.face_locations(imgS)
+            encodeCurFrame=face_recognition.face_encodings(imgS,faceCurLoc)
+
+            for encodeFace, faceLoc in zip(encodeCurFrame,faceCurLoc):
+                matches=face_recognition.compare_faces(knownEncodeList,encodeFace)
+                faceDis=face_recognition.face_distance(knownEncodeList,encodeFace)
+                # print("matches",matches)
+                # print("faceDis",faceDis)
+
+                
+                matchIndex=np.argmin(faceDis)
+                print("FacDisList",faceDis)
+                print("FacDis index ",faceDis[matchIndex])
+
+                # print("matchIndex",matchIndex)
+                # print(type(faceDis))
+
+                A, B = np.partition(faceDis, 1)[0:2]
+                # print("1st = ",A)
+                # print("2nd = ",B)
+                y1,x2,y2,x1=faceLoc
+                y1,x2,y2,x1=y1*4,x2*4,y2*4,x1*4
+                bbox= x1,y1,x2-x1,y2-y1
+                
+                
+                if (matches[matchIndex]==True and A<B and A<=0.60):
+                    print("knownFace dis: ", faceDis[matchIndex])
+                    cvzone.cornerRect(frame,bbox,rt=1,t=5,colorR=(220,218,168),colorC=(0,255,0))
+                    cv.putText(frame,str(studentIDs[matchIndex]),(50+x1,140+y1-10),cv.FONT_HERSHEY_SIMPLEX,color=(0,255,0),fontScale=1,thickness=1)
+
+                else:
+                # print("UnKnownFace")
+                    cvzone.cornerRect(frame,bbox,rt=1,t=5,colorR=(70,57,230),colorC=(0,255,0))
+
+                    cv.putText(frame,'unKnown',(50+x1,140+y1-10),cv.FONT_HERSHEY_SIMPLEX,color=(70,57,230),fontScale=1,thickness=1)
+            
+            #end of face rec....
             ret,buffer=cv2.imencode('.jpg',frame)
             frame=buffer.tobytes()
 
@@ -19,10 +73,22 @@ def generate_frames():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
+
+
+
 @app.route('/')
 def index():
     print("index")
-    return render_template('list.html')
+    mycursor.execute('''SELECT asheet.id, student.name, student.roll_no, course.name, teacher.name, asheet.date, asheet.lec_num, asheet.attendance_status FROM attendance_sheet asheet
+    INNER JOIN student ON asheet.student_id = student.id
+    INNER JOIN course ON asheet.course_id = course.id
+    INNER JOIN teacher ON asheet.teacher_id = teacher.id
+    ORDER BY asheet.lec_num DESC''')
+    data = mycursor.fetchall()
+    # print(data)
+    
+    return render_template('list.html', attSheet=data)
+
 
 @app.route('/video')
 def video():
--- mySQL.py
+++ mySQL.py
@@ -4,7 +4,7 @@ dbconn = mysql.connector.connect(
     port=3306,
     user="root",
     password="",
-    database="attendence_system"
+    database="attendance_system"
 
 )
 
@@ -13,3 +13,15 @@ if (dbconn):
     print("DB successfull")
 else:
     print("DB not successfull")
+# SELECT asheet.id, student.name, student.roll_no, course.course_name, teacher.teacher_name, asheet.date, asheet.lec_num, asheet.attendance_status FROM attendance_sheet asheet
+# INNER JOIN student ON asheet.student_id = student.id
+# INNER JOIN course ON asheet.course_id = course.id
+# INNER JOIN teacher ON asheet.teacher_id = teacher.id
+
+mycursor.execute('''SELECT asheet.id, student.name, student.roll_no, course.name, teacher.name, asheet.date, asheet.lec_num, asheet.attendance_status FROM attendance_sheet asheet 
+INNER JOIN student ON asheet.student_id = student.id
+INNER JOIN course ON asheet.course_id = course.id
+INNER JOIN teacher ON asheet.teacher_id = teacher.id
+ORDER BY asheet.lec_num DESC''')
+data = mycursor.fetchall()
+print(data)
